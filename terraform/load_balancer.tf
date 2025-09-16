9ceb73633a293a2fd61e1171afbb897dcf9f3239
Sergio Lopes Silva Neto <sergiolneto@gmail.com>
2025-09-15 21:46:21 -0300

    Fix: Configurando o load Balance

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -72,15 +72,21 @@ resource "aws_lb_target_group" "frontend" {
   }
 }
 
-# Data source for the existing HTTP listener
-data "aws_lb_listener" "http" {
+# Listener for the ALB
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
 }
 
 # Listener rule to forward API traffic to the backend
 resource "aws_lb_listener_rule" "api" {
-  listener_arn = data.aws_lb_listener.http.arn
+  listener_arn = aws_lb_listener.http.arn
   priority     = 100
 
   action {
